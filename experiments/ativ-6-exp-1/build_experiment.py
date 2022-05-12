28090260f0f9ba6586be047550dcae3139cdd604
AlanGanem <ganem.alan@hotmail.com>
2022-05-12 00:25:48 -0300

    passing commands as list now

--- experiments/ativ-6-exp-1/build_experiment.py
+++ experiments/ativ-6-exp-1/build_experiment.py
@@ -1,10 +1,14 @@
 import subprocess
 
 if __name__ == "__main__":
-    COMMAND = "sudo apt update ;" +\ 
-        "sudo apt install -y docker.io ; " +\
-        "cd ../.. ; mkdir cifar10 && cd cifar10 ; " + \
-        "wget --no-check-certificate https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz ; " + \
-        "tar -xvf cifar-10-python.tar.gz ; " + \
-        "sudo docker build -t dist_dcgan ."
+    COMMAND = [
+        "sudo apt update ;",
+        "sudo apt install -y docker.io ; ",
+        "cd ../.. ; mkdir cifar10 && cd cifar10 ; ",
+        "wget --no-check-certificate https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz ; ",
+        "tar -xvf cifar-10-python.tar.gz ; ",
+        "sudo docker build -t dist_dcgan .",
+    ]
+    
+    COMMAND = ''.join(COMMAND)
     subprocess.run(COMMAND, shell = True)
