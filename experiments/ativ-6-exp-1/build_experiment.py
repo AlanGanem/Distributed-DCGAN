d165db535c80ded47f0e7690804928058d8fbe10
AlanGanem <ganem.alan@hotmail.com>
2022-05-11 23:56:53 -0300

    fixed command typo

--- experiments/ativ-6-exp-1/build_experiment.py
+++ experiments/ativ-6-exp-1/build_experiment.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     COMMAND = "cd ../.. ; mkdir cifar10 && cd cifar10 ; " + \
         "wget --no-check-certificate https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz ; " + \
         "tar -xvf cifar-10-python.tar.gz ; " + \
-        "cd .." + \
+        "cd .. ; " + \
         "sudo apt install docker.io ; " +\
         "docker build -t dist_dcgan ."
     subprocess.run(COMMAND, shell = True)
