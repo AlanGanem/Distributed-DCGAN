9af9ac2b73cc18f86ab92196d94b61d041973bd1
AlanGanem <ganem.alan@hotmail.com>
2022-05-12 00:22:59 -0300

    added sudo apt udpate to build script

--- experiments/ativ-6-exp-1/build_experiment.py
+++ experiments/ativ-6-exp-1/build_experiment.py
@@ -1,8 +1,8 @@
 import subprocess
 
 if __name__ == "__main__":
-    COMMAND = "sudo apt update" +\ 
-    "sudo apt install -y docker.io ; " +\
+    COMMAND = "sudo apt update ;" +\ 
+        "sudo apt install -y docker.io ; " +\
         "cd ../.. ; mkdir cifar10 && cd cifar10 ; " + \
         "wget --no-check-certificate https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz ; " + \
         "tar -xvf cifar-10-python.tar.gz ; " + \
